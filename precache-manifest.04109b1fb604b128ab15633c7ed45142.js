b1bc7c4a1c3b6ad6837bd6e5dd93a897f80a19fd
Shazad0304 <shazadshiraz97@gmail.com>
2020-11-29 07:10:10 +0500

    Updates

---
+++ precache-manifest.04109b1fb604b128ab15633c7ed45142.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd9979a4f5701d8df03fe204cd39640c",
+    "url": "/ems-frontend/index.html"
+  },
+  {
+    "revision": "ea066976ab56456a07c1",
+    "url": "/ems-frontend/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "9bb6707ac7c79a2a356c",
+    "url": "/ems-frontend/static/css/main.db31c2d3.chunk.css"
+  },
+  {
+    "revision": "ea066976ab56456a07c1",
+    "url": "/ems-frontend/static/js/2.539874e3.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/ems-frontend/static/js/2.539874e3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9bb6707ac7c79a2a356c",
+    "url": "/ems-frontend/static/js/main.b1f625ee.chunk.js"
+  },
+  {
+    "revision": "64d1f6a15465c333db3e",
+    "url": "/ems-frontend/static/js/runtime-main.9b179547.js"
+  }
+]);
